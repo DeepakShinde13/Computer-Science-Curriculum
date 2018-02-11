3fd0b760eb1bec926571158fce4362be015b292d
Deepak Shinde <dkshinde13@gmail.com>
2018-02-11 19:44:30 +0530

    Insertion sort and Quick Sort

---
+++ Algorithms-and-data-structures/quicksort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void print_array(int a[], int n){
+    for(int i = 0; i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+void quick_sort(int a[], int n){
+
+    for
+    print_array(a,n);
+
+}
+
+int main(){
+    int a [] = {-2,-10,7,8,2,1,3,9,5};
+    quick_sort(a, sizeof(a)/sizeof(*a));
+    return 0;
+}
