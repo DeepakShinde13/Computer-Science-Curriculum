5e2898b7b94fa102b5ae151d5fdc241c4a89d715
Deepak Shinde <dkshinde13@gmail.com>
2018-02-27 01:17:10 +0530

    Doubly Linked list added

--- Algorithms-and-data-structures/Linked-list.cpp
+++ Algorithms-and-data-structures/Linked-list.cpp
@@ -8,28 +8,28 @@ struct node {
 };
 struct node *head = NULL;
 
-void displayLinkedList(){
-    cout<<"Link List : ";
+void displayList(){
+    cout<<"Linked List: ";
     struct node *temp = head;
-    if(head == NULL){
+    if(head==NULL){
         return;
     }
-    while(temp->next != NULL ){
-        cout<<temp->data << "-->";
+    while(temp->next !=NULL){
+        cout<< temp->data << "-->";
         temp = temp->next;
     }
-    cout << temp->data << endl;
-
+    cout<<temp->data<<endl;
 }
-void insertNode(int val){
 
-    cout<<"Inserting... "<<val<< "\t";
-    struct node* new_node = (struct node*) malloc (sizeof(struct node));
-    new_node->data = val;
+void insertNode(int val){
+    cout<<"Inserting "<< val <<"...\t";
+    struct node *new_node = (struct node*) malloc(sizeof(struct node));
+    new_node->data= val;
     new_node->next = NULL;
 
-    if ( head == NULL){
+    if(head == NULL){
         head = new_node;
+
     }
     else{
         struct node *temp = head;
@@ -38,69 +38,71 @@ void insertNode(int val){
         }
         temp->next = new_node;
     }
+    displayList();
 
-
-    //displaying the list
-    displayLinkedList();
 }
+
 void deleteNode(int val){
-    cout << "Deleting... " << val <<"\t";
+    cout<<"Deleting "<<val<<"...\t";
     struct node *temp = head;
-    if (head == NULL){
-        cout<<"Oops.. List is empty"<<endl;
+    if(head==NULL){
+        cout<<"Oops list is empty"<<endl;
         return;
     }
-
-    while(temp->next!= NULL){
-        if(temp->next->data == val){
-            struct node *node_to_be_removed = temp->next;
-            temp->next = node_to_be_removed->next;
-            node_to_be_removed = NULL;
-            free(node_to_be_removed);
-            displayLinkedList();
-            return;
+    else{
+        while(temp->next != NULL){
+            if(temp->next->data == val){
+                struct node *node_to_be_freed = temp->next;
+                temp->next = temp->next->next;
+                node_to_be_freed->next = NULL;
+                free(node_to_be_freed);
+                break;
+            }
+            temp = temp->next;
         }
-        temp = temp->next;
+        if(temp->next == NULL){
+            cout << "Error : Value "<< val<< " doesn't exist in Linked Listt"<<endl;
+        }
     }
 
-    cout<<"Error! "<<val<<" doesn't exist in the list"<<endl;
+    displayList();
 }
 
 void searchNode(int val){
-    cout<<"Searching "<< val <<"\t";
-
+    cout<<"Searching "<<val<<"...\t";
     struct node *temp = head;
-    if (head == NULL){
-        cout<<"Oops.. List is empty"<<endl;
+    if(head==NULL){
+        cout<<"Oops list is empty"<<endl;
         return;
     }
-
-    int count = 0;
-    while(temp->next!= NULL){
-        if(temp->next->data == val){
-            cout << "Success! "<< val<<" found at index "<<count <<endl;
-            return;
+    else{
+        int count = 0;
+        while(temp!= NULL){
+            if(temp->data == val){
+                cout<<"Success : Value "<< val << " exists at location : "<<count<<endl;
+                return;
+            }
+            temp = temp->next;
+            count += 1;
         }
-        temp = temp->next;
-        count+=1;
+        cout<<"Error : Value "<<val<<" doesn't exist in Linked List"<<endl;
     }
-
-    cout<<"Error! "<<val<<" doesn't exist in the list"<<endl;
-
-    }
-
+}
 int main(){
-    int a[] = {7, 2, 5, 6, 73, 4, 25, 26};
-    for(int i=0; i< sizeof(a)/sizeof(*a);i++){
-        //inserting a node;
-        insertNode(a[i]);
-    }
-    //deleting a node;
-    deleteNode(2);
-    deleteNode(100);
+    int a []= {1,3,4,5,6,7,8,2};
 
-    //searching a node
-    searchNode(73);
-    searchNode(200);
-    return 0;
+	for(int i=0; i<(sizeof(a)/sizeof(*a)); i++){
+
+	    //Single Link List Insertion
+	    insertNode(a[i]);
+
+	}
+
+	//deleting node in SLL
+	deleteNode(4);
+
+	//searching a node in SLL
+    searchNode(8);
+
+	return 0;
 }
